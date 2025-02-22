a1b628aabbb59f488e685c544c4ce0562e90c6b9
Juanda1285 <juanda1284@gmail.com>
2025-02-22 13:06:49 -0500

    i18n / character detail , see more

--- src/app/pages/character-detail/character-detail.component.ts
+++ src/app/pages/character-detail/character-detail.component.ts
@@ -5,22 +5,25 @@ import { MatCardModule } from '@angular/material/card';
 import { DragonBallService } from '../../services/dragon-ball.service';
 import { TruncatePipe } from '../../pipes/truncate.pipe';
 import { MatIcon } from '@angular/material/icon';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-character-detail',
   templateUrl: './character-detail.component.html',
   styleUrls: ['./character-detail.component.css'],
   standalone: true,
-  imports: [CommonModule, MatCardModule, TruncatePipe, MatIcon, TranslateModule],
+  imports: [CommonModule, MatCardModule, TruncatePipe, MatIcon, MatButtonModule, TranslateModule],
 })
 export class CharacterDetailComponent implements OnInit {
   character: any;
+  showFullDescription: { [key: number]: boolean } = {};
 
   constructor(
     private route: ActivatedRoute,
     private dragonBallService: DragonBallService,
-    private location: Location
+    private location: Location,
+    public translate: TranslateService
   ) {}
 
   ngOnInit(): void {
@@ -34,5 +37,17 @@ export class CharacterDetailComponent implements OnInit {
 
   goBack(): void {
     this.location.back();
+  }
+
+  toggleDescription(characterId: number) {
+    this.showFullDescription[characterId] = !this.showFullDescription[characterId];
+  }
+
+  isFullDescriptionShown(characterId: number): boolean {
+    return this.showFullDescription[characterId] || false;
+  }
+
+  getTranslation(key: string): string {
+    return this.translate.instant(key);
   }
 }
